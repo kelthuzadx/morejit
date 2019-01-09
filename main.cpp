c64bfde9f8a1d434dc72752899ead5713f20d273
racaljk <1948638989@qq.com>
2019-01-09 22:04:23 +0800

    x86: mov,

--- main.cpp
+++ main.cpp
@@ -2,49 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cstdint>
 #include <iostream>
 
 typedef int (*puts_ptr)(const char*);
 typedef void (*myfunc_ptr)(puts_ptr);
 
-/*
-void foo(puts_ptr p) {
-    p("greetings from generated code!");
-}
-*/
 #if 0
 offset | bytes (in hex) | mnemonics
 00     | 55             | push EBP
 01     | 8BEC           | mov  EBP, ESP
 03     | 68 ????????    | push ???????? ; address of string not known untilrun-time 
 08     | FF55 08        | call dword ptr [EBP + 8] 
-11     | 83C4 0400        |add  ESP, 4 
+11     | 81C4 0400        |add  ESP, 4 
 15     | 5D             | pop  EBP 
 16     | C3             | ret
 #endif
-#include <cstdint>
-
-#define imm32(X) (X)
+/// All instructions are destination operand first. For example, give a
+/// mov_reg32_reg32(ESP,EBP), it will generate mov esp<-ebp
 
 using i8 = std::int8_t;
+using u8 = std::uint32_t;
 enum Register { EAX = 0, ECX, EDX, EBX, ESP, EBP, ESI, EDI };
 
-#define _IMM32_TO_CODE(CODE, I)                   \
-    *CODE++ = (i8)(imm32(I) & 0XFF);              \
-    *CODE++ = (i8)((imm32(I) & 0xFF00) >> 8);     \
-    *CODE++ = (i8)((imm32(I) && 0xFF0000) >> 16); \
-    *CODE++ = (i8)((imm32(I) && 0xFF000000) >> 24);
+#define _modrm(MOD, REG, RM) \
+    (u8)((RM & 0x7) | ((REG & 0x7) << 3) | (0b##MOD << 6))
+
+#define _imm32_to_code(CODE, I)           \
+    *CODE++ = (i8)(I & 0XFF);             \
+    *CODE++ = (i8)((I & 0xFF00) >> 8);    \
+    *CODE++ = (i8)((I & 0xFF0000) >> 16); \
+    *CODE++ = (i8)((I & 0xFF000000) >> 24);
+
+#define mov_reg32_reg32(CODE, R1, R2) \
+    *CODE++ = 0x8b;                   \
+    *CODE++ = _modrm(11, R1, R2);
+
+#define mov_reg32_imm32(CODE, R, I) \
+    *CODE++ = (0xb8 + R);           \
+    _imm32_to_code(CODE, I);
 
 #define add_reg32_imm32(CODE, R, I) \
     *CODE++ = 0x81;                 \
-    *CODE++ = (0xC0 + R);           \
-    _IMM32_TO_CODE(CODE, I)
+    *CODE++ = _modrm(11, 000, R);   \
+    _imm32_to_code(CODE, I)
 
-#define push_reg(CODE, R) *CODE++ = (0x50 + R)
-#define pop_reg(CODE, R) *CODE++ = (0x58 + R)
+#define push_reg(CODE, R) *CODE++ = (0x50 + R);
+#define pop_reg(CODE, R) *CODE++ = (0x58 + R);
 #define push_imm32(I) \
     *CODE++ = 0x68;   \
-    _IMM32_TO_CODE(CODE, I)
+    _imm32_to_code(CODE, I)
 
 #define ret(CODE) *CODE++ = (0xC3)
 
@@ -58,7 +65,10 @@ void deallocateArean(void* addr) { VirtualFree(addr, 0, MEM_RELEASE); }
 void f(int a, int b, int c) { std::cout << a << b << b; }
 
 int main() {
-    f(5325, 235235, 2);
+    char* p = (char*)malloc(100);
+    int k = _modrm(11, 0, ESP);
+    mov_reg32_imm32(p, EDX, 42);
+    f(5325, 235235, k);
     myfunc_ptr pMyfunc; /* pointer to generated code */
     puts_ptr pPuts = &puts;
     char* code = (char*)allocateArean(50);
@@ -70,9 +80,8 @@ int main() {
 
     /* function prologue */
     push_reg(gen, EBP);
+    mov_reg32_reg32(gen, EBP, ESP);
 
-    *gen++ = 0x8B; /* mov EBP, ESP             */
-    *gen++ = 0xEC;
     /* function body */
     *gen++ = 0x68;                 /* push                     */
     *((int*)gen) = (int)code + 19; /* (address of string)      */
