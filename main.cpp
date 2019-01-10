fe39786c26c2d950db473a822ad4fae4d1b34dda
racaljk <1948638989@qq.com>
2019-01-10 16:54:10 +0800

    x86: fix push_imm32

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@
 #include <cstdint>
 #include <iostream>
 
-typedef int (*puts_ptr)(const char*);
-typedef void (*myfunc_ptr)(puts_ptr);
-
 #if 0
 offset | bytes (in hex) | mnemonics
 00     | 55             | push EBP
@@ -17,24 +14,36 @@ offset | bytes (in hex) | mnemonics
 11     | 81C4 0400      | add  ESP, 4 
 15     | 5D             | pop  EBP 
 16     | C3             | ret
-#endif
-#if 0
+
    printf("%d%d%d", 5325, 235235, k);
 003D50B1 8B 45 EC             mov         eax,dword ptr [k]  
 003D50B4 50                   push        eax  
 003D50B5 68 E3 96 03 00       push        396E3h  
 003D50BA 68 CD 14 00 00       push        14CDh  
 003D50BF 68 30 7B 3D 00       push        offset string "%d%d%d" (03D7B30h)  
-003D50C4 E8 E3 C2 FF FF       call        _printf (03D13ACh)  
+003D50C4 E8 FF FF       call        _printf (03D13ACh)  
 003D50C9 83 C4 10             add         esp,10h
 #endif
 
 /// All instructions are destination operand first. For example, give a
 /// mov_reg32_reg32(ESP,EBP), it will generate mov esp<-ebp
 
+typedef void (*func_ptr)(int __cdecl printf(char const* const, ...), int, int);
+
 using i8 = std::int8_t;
 using u8 = std::uint32_t;
+using code_ptr = i8*;
 enum Register { EAX = 0, ECX, EDX, EBX, ESP, EBP, ESI, EDI };
+
+static void* allocate_arean(size_t size) {
+    return VirtualAlloc(NULL, 50, MEM_COMMIT | MEM_RESERVE,
+                        PAGE_EXECUTE_READWRITE);
+}
+
+static void deallocate_arean(void* addr) { VirtualFree(addr, 0, MEM_RELEASE); }
+
+code_ptr init_jit_code(size_t size) { return (code_ptr)allocate_arean(size); }
+void free_jit_code(code_ptr code) { deallocate_arean(code); }
 
 #define _modrm(MOD, REG, RM) \
     (u8)((RM & 0x7) | ((REG & 0x7) << 3) | (0b##MOD << 6))
@@ -44,6 +53,10 @@ enum Register { EAX = 0, ECX, EDX, EBX, ESP, EBP, ESI, EDI };
     *CODE++ = (i8)((I & 0xFF00) >> 8);    \
     *CODE++ = (i8)((I & 0xFF0000) >> 16); \
     *CODE++ = (i8)((I & 0xFF000000) >> 24);
+
+#define call(CODE, I) \
+    *CODE++ = 0xe8;   \
+    imm32(CODE, I);
 
 // call [eax+dispatch8]
 #define call_8(CODE, R, DISP)   \
@@ -66,51 +79,42 @@ enum Register { EAX = 0, ECX, EDX, EBX, ESP, EBP, ESI, EDI };
 
 #define push_reg(CODE, R) *CODE++ = (0x50 + R);
 #define pop_reg(CODE, R) *CODE++ = (0x58 + R);
-#define push_imm32(I) \
-    *CODE++ = 0x68;   \
+#define push_imm32(CODE, I) \
+    *CODE++ = 0x68;         \
     _imm32(CODE, I)
 
 #define ret(CODE) *CODE++ = (0xC3)
 
-void* allocateArean(size_t size) {
-    return VirtualAlloc(NULL, 50, MEM_COMMIT | MEM_RESERVE,
-                        PAGE_EXECUTE_READWRITE);
-}
-
-void deallocateArean(void* addr) { VirtualFree(addr, 0, MEM_RELEASE); }
-
 int main() {
-    __asm {
-         mov ebx,esp
-    }
-    char* p = (char*)malloc(100);
-    int k = _modrm(11, 0, ESP);
-    mov_reg32_imm32(p, EDX, 42);
-    printf("%d%d%d", 5325, 235235, k);
     // Startup
-    char* code = (char*)allocateArean(500);
+    code_ptr code = init_jit_code(500);
 
-    char* gen = code; /* address to generate code to */
-    const char* pStr = "generating on the fly";
+    const char* format = "Got %d,%d";
+    strcpy((char*)code, format);
+    code[strlen(format)] = '\0';
 
-    /* function prologue */
+    code_ptr start = code + strlen(format) + 1;
+    code_ptr gen = start;
+    // prologue
     push_reg(gen, EBP);
     mov_reg32_reg32(gen, EBP, ESP);
 
-    /* function body */
-    *gen++ = 0x68;                 /* push                     */
-    *((int*)gen) = (int)code + 19; /* (address of string)      */
-    gen += 4;
+    push_imm32(gen, 0XAB);
+    push_imm32(gen, 0XCD);
+    push_imm32(gen, (int)format);
     mov_reg32_reg32(gen, EBX, EBP);
     call_8(gen, EBX, 8);  // call [esp+8]
-    add_reg32_imm32(gen, ESP, 0x4);
+
+    add_reg32_imm32(gen, ESP, 12);
+
+    // epilogue
     pop_reg(gen, EBP);
     ret(gen);
-    strcpy(gen, pStr);
-    /* end of code generation */
 
-    myfunc_ptr pMyfunc = (myfunc_ptr)code;
-    pMyfunc(&puts);
-    deallocateArean(code);
+    func_ptr pMyfunc = (func_ptr)(start);
+    pMyfunc(&printf, 0XAB, 0XCD);
+
+    free_jit_code(code);
+
     return 0;
 }
